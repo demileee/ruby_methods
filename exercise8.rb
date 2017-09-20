9d7d67958d9fd98287bfe5d9b896d3cde53b245a
Demi Lee <demilee.to@gmail.com>
2017-09-20 10:52:59 -0400

    employed dry

--- exercise8.rb
+++ exercise8.rb
@@ -1,32 +1,27 @@
-puts "How far did person 1 run (in metres)?"
-dis1 = gets.to_f
-puts "How long (in minutes) did person 1 run take to run #{dis1} metres?"
-mins1 = gets.to_f
-
-
-puts "How far did person 2 run (in metres)?"
-dis2 = gets.to_f
-puts "How long (in minutes) did person 2 take to run #{dis2} metres?"
-mins2 = gets.to_f
-
-
-puts "How far did person 3 run (in metres)?"
-dis3 = gets.to_f
-puts "How long (in minutes) did person 3 take to run #{dis3} metres?"
-mins3 = gets.to_f
-
-def speed(x,y)
-  z = x / (y*60)
+def input(x)
+  puts "How far did person #{x} run (in metres)?"
+  dis = gets.to_f
+  puts "How long (in minutes) did person #{x} run take to run #{dis} metres?"
+  mins = gets.to_f
+  speed = dis / (mins*60)
 end
 
-if speed(dis3,mins3) > speed(dis2,mins2) && speed(dis3,mins3) > speed(dis1,mins1)
-  puts "Person 3 was the fastest at #{speed(dis3,mins3)} m/s."
-elsif speed(dis2,mins2) > speed(dis3,mins3) && speed(dis2,mins2) > speed(dis1,mins1)
-  puts "Person 2 was the fastest at #{speed(dis2,mins2)} m/s."
-elsif speed(dis1,mins1) > speed(dis3,mins3) && speed(dis1,mins1) > speed(dis2,mins2)
-  puts "Person 1 was the fastest at #{speed(dis1,mins1)} m/s."
+def result(x, y)
+  puts "Person #{x} was the fastest at #{y} m/s."
+end
+
+speed1 = input(1)
+speed2 = input(2)
+speed3 = input(3)
+
+if speed3 > speed2 && speed3 > speed1
+  puts result(3, speed3)
+elsif speed2 > speed3 && speed2 > speed1
+  puts result(2, speed2)
+elsif speed1 > speed3 && speed1 > speed2
+  puts result(3, speed1)
 else
-  puts "Everyone tied at #{speed(dis1,mins1)} m/s."
+  puts "Everyone tied at #{speed1} m/s."
 end
 
 puts "Well done, everyone!"
